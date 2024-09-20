03a641da0748e5625fc49ea050c7549d97d6f262
Glenn G. Chappell <ggchappell@alaska.edu>
2024-09-19 21:44:09 -0800

    Add da3.hpp/.cpp (SKELETON)

---
+++ da3.cpp
@@ -0,0 +1,27 @@
+// da3.cpp  SKELETON
+// Glenn G. Chappell
+// 2024-09-19
+//
+// For CS 311 Fall 2024
+// Source for Assignment 3 functions
+
+#include "da3.hpp"     // For Assignment 3 prototypes & templates
+
+#include <functional>
+using std::function;
+
+
+void didItThrow(const function<void()> & ff,
+                bool & threw)
+{
+    // TODO: WRITE THIS!!!
+}
+
+
+int gcd(int a,
+        int b)
+{
+    return 42;  // DUMMY
+    // TODO: WRITE THIS!!!
+}
+
