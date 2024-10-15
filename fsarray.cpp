f3167eb2918ea2033bc1892028f637490fe45fb2
Glenn G. Chappell <ggchappell@alaska.edu>
2024-10-15 11:51:11 -0800

    Add fsarray.hpp (UNFINISHED), fsarray.cpp (UNFINISHED)

---
+++ fsarray.cpp
@@ -0,0 +1,18 @@
+// fsarray.cpp  UNFINISHED
+// Glenn G. Chappell
+// 2024-10-15
+//
+// For CS 311 Fall 2024
+// Source for class FSArray
+// Frightfully smart array of int
+// Preliminary to Assignment 5
+
+
+#include "fsarray.hpp"  // For class FSArray definition
+
+
+// *********************************************************************
+// class FSArray - Member function definitions
+// *********************************************************************
+
+
---
+++ fsarray.hpp
@@ -0,0 +1,50 @@
+// fsarray.hpp  UNFINISHED
+// Glenn G. Chappell
+// 2024-10-15
+//
+// For CS 311 Fall 2024
+// Header for class FSArray
+// Frightfully smart array of int
+// Preliminary to Assignment 5
+
+#ifndef FILE_FSARRAY_HPP_INCLUDED
+#define FILE_FSARRAY_HPP_INCLUDED
+
+
+// *********************************************************************
+// class FSArray - Class definition
+// *********************************************************************
+
+
+// class FSArray
+// Frightfully Smart Array of int.
+// Resizable, copyable/movable, exception-safe.
+// Invariants:
+//     ???
+class FSArray {
+
+// ***** FSArray: types *****
+public:
+
+
+// ***** FSArray: ctors, op=, dctor *****
+public:
+
+
+// ***** FSArray: general public operators *****
+public:
+
+
+// ***** FSArray: general public functions *****
+public:
+
+
+// ***** FSArray: data members *****
+private:
+
+
+};  // End class FSArray
+
+
+#endif  //#ifndef FILE_FSARRAY_HPP_INCLUDED
+
