85a41062474e3eb522e79c996b3866c42c2fcb2f
Glenn G. Chappell <ggchappell@alaska.edu>
2024-10-22 13:26:01 -0800

    fsarray.hpp/.cpp: VERSION 5: - Move func defs to source file: copy & move ops, resize, insert,   erase, swap.

--- fsarray.cpp
+++ fsarray.cpp
@@ -1,8 +1,8 @@
 // fsarray.cpp  UNFINISHED
-// VERSION 4
+// VERSION 5
 // Glenn G. Chappell
 // Started: 2024-10-15
-// Updated: 2024-10-21
+// Updated: 2024-10-22
 //
 // For CS 311 Fall 2024
 // Source for class FSArray
@@ -11,10 +11,89 @@
 
 
 #include "fsarray.hpp"  // For class FSArray definition
+#include <algorithm>
+using std::copy;
 
 
 // *********************************************************************
 // class FSArray - Member function definitions
 // *********************************************************************
 
+
+// Copy ctor
+// See header for info.
+FSArray::FSArray(const FSArray & other)
+    :_size(other.size()),
+     _data(other.size() == 0 ? nullptr
+                             : new value_type[other.size()])
+{
+    copy(other.begin(), other.end(), begin());
+    // The above call to std::copy does not throw, since it copies int
+    // values. But if value_type is changed, then the call may throw, in
+    // which case this copy ctor may need to be rewritten.
+}
+
+
+// Move ctor
+// See header for info.
+FSArray::FSArray(FSArray && other) noexcept
+    :_size(other._size),
+     _data(other._data)
+{
+    other._size = 0;
+    other._data = nullptr;
+}
+
+
+// Copy assignment operator
+// See header for info.
+FSArray & FSArray::operator=(const FSArray & other)
+{
+    // TODO: WRITE THIS!!!
+    return *this; // DUMMY
+}
+
+
+// Move assignment operator
+// See header for info.
+FSArray & FSArray::operator=(FSArray && other) noexcept
+{
+    // TODO: WRITE THIS!!!
+    return *this; // DUMMY
+}
+
+
+// resize
+// See header for info.
+void FSArray::resize(FSArray::size_type newsize)
+{
+    // TODO: WRITE THIS!!!
+}
+
+
+// insert
+// See header for info.
+FSArray::iterator FSArray::insert(FSArray::iterator pos,
+                                  FSArray::value_type item)
+{
+    // TODO: WRITE THIS!!!
+    return begin();  // DUMMY
+}
+
+
+// erase
+// See header for info.
+FSArray::iterator FSArray::erase(FSArray::iterator pos)
+{
+    // TODO: WRITE THIS!!!
+    return begin();  // DUMMY
+}
+
+
+// swap
+// See header for info.
+void FSArray::swap(FSArray & other) noexcept
+{
+    // TODO: WRITE THIS!!!
+}
 
--- fsarray.hpp
+++ fsarray.hpp
@@ -1,8 +1,8 @@
 // fsarray.hpp  UNFINISHED
-// VERSION 4
+// VERSION 5
 // Glenn G. Chappell
 // Started: 2024-10-15
-// Updated: 2024-10-21
+// Updated: 2024-10-22
 //
 // For CS 311 Fall 2024
 // Header for class FSArray
@@ -31,14 +31,15 @@
 //   - Revise ctor from size, copy ctor accordingly.
 //   - Write move ctor.
 //   - Mark various functions as noexcept.
+// - v5:
+//   - Move func defs to source file: copy & move ops, resize, insert,
+//     erase, swap.
 
 #ifndef FILE_FSARRAY_HPP_INCLUDED
 #define FILE_FSARRAY_HPP_INCLUDED
 
 #include <cstddef>
 // For std::size_t
-#include <algorithm>
-// For std::copy
 
 
 // *********************************************************************
@@ -82,42 +83,19 @@ public:
 
     // Copy ctor
     // Strong Guarantee
-    FSArray(const FSArray & other)
-        :_size(other.size()),
-         _data(other.size() == 0 ? nullptr
-                                 : new value_type[other.size()])
-    {
-        std::copy(other.begin(), other.end(), begin());
-        // The above call to std::copy does not throw, since it copies
-        // int values. But if value_type is changed, then the call may
-        // throw, in which case this copy ctor may need to be rewritten.
-    }
+    FSArray(const FSArray & other);
 
     // Move ctor
     // No-Throw Guarantee
-    FSArray(FSArray && other) noexcept
-        :_size(other._size),
-         _data(other._data)
-    {
-        other._size = 0;
-        other._data = nullptr;
-    }
+    FSArray(FSArray && other) noexcept;
 
     // Copy assignment operator
     // ??? Guarantee
-    FSArray & operator=(const FSArray & other)
-    {
-        // TODO: WRITE THIS!!!
-        return *this; // DUMMY
-    }
+    FSArray & operator=(const FSArray & other);
 
     // Move assignment operator
     // No-Throw Guarantee
-    FSArray & operator=(FSArray && other) noexcept
-    {
-        // TODO: WRITE THIS!!!
-        return *this; // DUMMY
-    }
+    FSArray & operator=(FSArray && other) noexcept;
 
     // Dctor
     // No-Throw Guarantee
@@ -183,34 +161,23 @@ public:
 
     // resize
     // ??? Guarantee
-    void resize(size_type newsize)
-    {
-        // TODO: WRITE THIS!!!
-    }
+    void resize(size_type newsize);
 
     // insert
     // Pre:
     //     ???
     // ??? Guarantee
     iterator insert(iterator pos,
-                    value_type item)
+                    value_type item);
         // Above, passing by value is appropriate, since our value type
         // is int. However, if the value type is changed, then a
         // different parameter-passing method may need to be used.
-    {
-        // TODO: WRITE THIS!!!
-        return begin();  // DUMMY
-    }
 
     // erase
     // Pre:
     //     ???
     // ??? Guarantee
-    iterator erase(iterator pos)
-    {
-        // TODO: WRITE THIS!!!
-        return begin();  // DUMMY
-    }
+    iterator erase(iterator pos);
 
     // push_back
     // ??? Guarantee
@@ -233,10 +200,7 @@ public:
 
     // swap
     // No-Throw Guarantee
-    void swap(FSArray & other) noexcept
-    {
-        // TODO: WRITE THIS!!!
-    }
+    void swap(FSArray & other) noexcept;
 
 // ***** FSArray: data members *****
 private:
